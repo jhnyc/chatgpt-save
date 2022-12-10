0ffc6ef49475668e86b1b9d069fc2ca0f57042b7
Johnny <jhnyc@connect.hku.hk>
2022-12-09 22:55:39 -0800

    update manifest v3

--- background.js
+++ background.js
@@ -1,29 +1,7 @@
 // execute content script to parse prompts & responses
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, { file: "content.js" });
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["content.js"],
+    });
 });
-
-// listener for the message and a function to return the HTML of the page
-chrome.runtime.onMessage.addListener(function(request, sender, response) {
-    if (request.type == "result") {
-        console.log(request);
-        var csvBlob = createCSVBlob(request.data);
-        chrome.downloads.download({
-            url: URL.createObjectURL(csvBlob),
-            filename: "ChatGPT_History.csv",
-        });
-    }
-});
-
-// create csv from prompt & response arrays
-const createCSVBlob = (data) => {
-    const prompt = data[0];
-    const resp = data[1];
-    const csvHeader = "prompt,response\n";
-    var csvString = prompt
-        .map((p, i) => `${p},"${resp[i].replaceAll('"', '""')}"`) // handle comma,quote,line break
-        .join("\n");
-    console.log(csvString);
-    var csvBlob = new Blob([csvHeader + csvString], { type: "text/csv" });
-    return csvBlob;
-};
--- content.js
+++ content.js
@@ -1,18 +1,42 @@
-// get document elements of all prompts & responses
-var prompt = document.getElementsByClassName(
-    "w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800"
-);
+var getThreads = () => {
+    // get document elements of all prompts & responses
+    var prompt = document.getElementsByClassName(
+        "w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800"
+    );
+    var response = document.getElementsByClassName(
+        "w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group bg-gray-50 dark:bg-[#444654]"
+    );
+    // extract text from prompts & responses HTML elements
+    var promptData = Array.from(prompt).map((i) => i.textContent);
+    var responseData = Array.from(response).map((i) => i.textContent);
+    return [promptData, responseData];
+};
 
-var response = document.getElementsByClassName(
-    "w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group bg-gray-50 dark:bg-[#444654]"
-);
+// create csv from prompt & response arrays
+var createCSVBlob = (data) => {
+    const prompt = data[0];
+    const resp = data[1];
+    const csvHeader = "Prompt,Response\n";
+    var csvString = prompt
+        .map(
+            (p, i) =>
+            `"${p.replaceAll('"', '""')}","${resp[i].replaceAll('"', '""')}"`
+        ) // handle comma, quote, line break
+        .join("\n");
+    var csvBlob = new Blob([csvHeader + csvString], { type: "text/csv" });
+    return csvBlob;
+};
 
-// extract text from prompts & responses HTML elements
-var promptData = Array.from(prompt).map((i) => i.textContent);
-var responseData = Array.from(response).map((i) => i.textContent);
+var threads2CSV = () => {
+    var threads = getThreads();
+    var csvBlob = createCSVBlob(threads);
+    var url = URL.createObjectURL(csvBlob);
+    var temp = document.createElement("a");
+    temp.href = url;
+    temp.download = "ChatGPT_History.csv";
+    document.body.appendChild(temp);
+    temp.click();
+    document.body.removeChild(temp);
+};
 
-// send data to background.js
-chrome.runtime.sendMessage({
-    type: "result",
-    data: [promptData, responseData],
-});
+threads2CSV();
